247924bc16ef4a45a48381f601a94198c4f80e9b
Zach Williams <hey@zachwill.com>
2012-10-14 01:38:22 -0700

    Add UIColor category comments

--- Github/Categories/UIColor+Drinkups.m
+++ Github/Categories/UIColor+Drinkups.m
@@ -11,26 +11,25 @@
 @implementation UIColor (Drinkups)
 
 + (UIColor *)gh_backgroundColor {
-    return [UIColor colorWithRed:0.773 green:0.784 blue:0.745 alpha:1.000];
+    // #C5C7BD
+    return [UIColor colorWithRed:0.773 green:0.780 blue:0.741 alpha:1.000];
 }
 
 + (UIColor *)gh_refreshTintColor {
-    return [UIColor colorWithRed:0.631 green:0.647 blue:0.624 alpha:1.000];
+    // #A0A49F
+    return [UIColor colorWithRed:0.627 green:0.643 blue:0.624 alpha:1.000];
 }
 
 + (UIColor *)colorWithHex:(NSString *)hex {
     NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];  
 
-    // Strip 0X if it appears  
-    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
-    
-    // String pound sign
+    // Strip pound sign
     if ([cString hasPrefix:@"#"]) cString = [cString substringFromIndex:1];
     
     if(cString.length == 3) {
-        NSString *first = [cString substringWithRange:NSMakeRange(0, 1)];
+        NSString *first  = [cString substringWithRange:NSMakeRange(0, 1)];
         NSString *second = [cString substringWithRange:NSMakeRange(1, 1)];
-        NSString *third = [cString substringWithRange:NSMakeRange(2, 1)];
+        NSString *third  = [cString substringWithRange:NSMakeRange(2, 1)];
         cString = [NSString stringWithFormat:@"%@%@%@%@%@%@", first, first, second, second, third, third];
     }
 
